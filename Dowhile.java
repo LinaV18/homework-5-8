938fad1de595d7d121bc6b6d8005a3b02cc1de64
Lina Valand <linavaland@hotmail.co.uk>
2022-08-05 21:05:53 +0100

    Dowhile

---
+++ Dowhile.java
@@ -0,0 +1,14 @@
+package Dowhile;
+
+public class Dowhile {
+    public static void main(String[] args) {
+        int a=1;
+        do {
+            System.out.println(a);
+            a+=2;
+        }while(a<=10);
+
+        }
+
+    }
+
---
+++ Dowhileeven.java
@@ -0,0 +1,12 @@
+package Dowhile;
+
+public class Dowhileeven {
+    public static void main(String[] args) {
+        int a=2;
+        do {
+            System.out.println(a);
+            a+=2;
+        }while(a<=10);
+        }
+    }
+
